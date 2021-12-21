b93d1115e9d9e43b741611f4f9184dfee9374df4
Jang Hyeon yong <87103805+potato0520@users.noreply.github.com>
2021-12-21 14:15:57 +0900

    Develop (#1)
    * main.dart
    
    * cooking class page
    
    * food share page
    
    * new post page
    
    * shared recipe page
    
    * village page
    
    * .
    
    * main page
    
    * my profile apge
    
    * side bar + navigation button
    
    * 상당 타이틀 바  (폰트 추가 해야함)

---
+++ lib/screens/cookingclass.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CookingClass extends StatelessWidget {
+  const CookingClass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widgets/sidebar/sidebar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:neighborchefapp/screens/cookingclass.dart';
+import 'package:neighborchefapp/screens/foodshare.dart';
+import 'package:neighborchefapp/screens/newpost.dart';
+import 'package:neighborchefapp/screens/sharedrecipe.dart';
+import 'package:neighborchefapp/screens/village.dart';
+
+Widget sideBar(BuildContext context) {
+  var newpost = 'New Post';
+  var sharedrecipes = 'Shared Recipes';
+  var foodshare = 'Food share';
+  var cookingclass = 'Cooking Class';
+  var village = 'Village';
+
+  return Container(
+    width: MediaQuery.of(context).size.width * 2 / 3,
+    child: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Colors.white),
+            child: Text('empty'),
+          ),
+          navigationButton(context,
+              buttonname: newpost, icon: Icons.chat, id: NewPost()),
+          navigationButton(context,
+              buttonname: sharedrecipes,
+              icon: Icons.book_online,
+              id: SharedRecipe()),
+          navigationButton(context,
+              buttonname: foodshare, icon: Icons.food_bank, id: FoodShare()),
+          navigationButton(context,
+              buttonname: cookingclass, icon: Icons.class_, id: CookingClass()),
+          navigationButton(context,
+              buttonname: village, icon: Icons.home, id: Village()),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget navigationButton(BuildContext context,
+    {var buttonname = 'None Button', icon = Icons.error, var id = ''}) {
+  return SizedBox(
+    width: 400,
+    height: 60,
+    child: TextButton.icon(
+      icon: Icon(
+        (icon),
+        size: 30,
+        color: Colors.orange,
+      ),
+      label: Text(
+        buttonname,
+        style: TextStyle(color: Colors.black),
+      ),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => id));
+      },
+    ),
+  );
+}
