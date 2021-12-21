6d6cbc148ad6cd760d2dbd24054d258c1e21346d
potato0520 <potato20040520@gmail.com>
2021-12-21 14:06:48 +0900

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:neighborchefapp/screens/mainpage.dart';
+
+void main() {
+  runApp(const NeighborChef());
+}
+
+class NeighborChef extends StatelessWidget {
+  const NeighborChef({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: MainPage(),
+    );
+  }
+}
