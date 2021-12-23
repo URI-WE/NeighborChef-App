cf41ef55f267a2617b6fabd782a57b1b1e6eedea
Jang Hyeon yong <87103805+potato0520@users.noreply.github.com>
2021-12-24 00:59:34 +0900

    [추가] 추천 레시피  (#8)
    * [개발] Searchbar 위젯 외형 완성  기능 구현 해야됨
    
    * [추가] 새로 생긴 Searchbar 추가
    
    * [변경] 기존 sizedbox에서 listtile로 변경하여 위젯 정렬
    
    * feat: (추천 레시피)  추천 레시피 위젯 완성
    
    * chore:(위젯 추가) 추천 레시피 위젯 추가

---
+++ lib/widgets/etc/searchbar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget searchbar() {
+  return Padding(
+    padding: EdgeInsets.all(20),
+    child: TextField(
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.search,
+          color: Colors.black,
+          size: 30,
+        ),
+        hintText: 'Search',
+        hintStyle: TextStyle(color: Colors.grey, fontSize: 20),
+        filled: true,
+        fillColor: Colors.white,
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            borderSide: BorderSide(color: Colors.grey, width: 4)),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: Colors.grey, width: 4)),
+      ),
+    ),
+  );
+}
