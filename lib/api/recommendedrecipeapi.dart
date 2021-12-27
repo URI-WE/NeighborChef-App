08fbc30556ffe94495661dfe1e961b46e8aa0e0e
Jang Hyeon yong <87103805+potato0520@users.noreply.github.com>
2021-12-27 20:25:19 +0900

    [추가] 추천 레시피와 메인  (#13)
    * [개발] Searchbar 위젯 외형 완성  기능 구현 해야됨
    
    * [추가] 새로 생긴 Searchbar 추가
    
    * [변경] 기존 sizedbox에서 listtile로 변경하여 위젯 정렬
    
    * feat: (추천 레시피)  추천 레시피 위젯 완성
    
    * chore:(위젯 추가) 추천 레시피 위젯 추가
    
    * test
    
    * delete recommandedRecipe
    
    * .
    
    * [추가] 레시피 추천 Http 통신 배열
    
    * [수정] 위젯간의 거리 변경
    
    * [추가] 거색 위젯 개발
    
    * [수정] 추천 레시피 디자인 수정

---
+++ lib/api/recommendedrecipeapi.dart
@@ -0,0 +1,32 @@
+List<String> recipeName = [
+  'pankace',
+  'chiken',
+  'hong ramen',
+  'cheeze cake',
+  'apple pie',
+  'recipeName',
+];
+List<String> authorName = [
+  'Jang',
+  'Moon,' 'Kim',
+  'Lim',
+  'Jung',
+  'authorName',
+];
+List<String> description = [
+  'pancake is gold',
+  'human is first',
+  'gurger king',
+  'I am gay',
+  'Jung hwan',
+  'dexcription',
+];
+List<String> bookmark = ['true', 'false', 'flase', 'false', 'true'];
+List<String> recipePicture = [
+  'https://picsum.photos/250?image=1',
+  'https://picsum.photos/250?image=2',
+  'https://picsum.photos/250?image=3',
+  'https://picsum.photos/250?image=4',
+  'https://picsum.photos/250?image=5',
+  'none api',
+];
