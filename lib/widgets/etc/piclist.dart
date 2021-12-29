f8279221b3b994d2a523ba630020ca60f72dc508
kimkyungwon <openwind125@gmail.com>
2021-12-29 20:20:27 +0900

    Update piclist.dart

--- lib/widgets/etc/piclist.dart
+++ lib/widgets/etc/piclist.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-Widget _MyHomePageState(List<String> imageList) {
-  return StaggeredGridView.countBuilder(
-      crossAxisCount: 2,
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 12,
-      itemCount: imageList.length,
-      itemBuilder: (context, index) {
-        return Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(imageList[index]),
-              fit: BoxFit.cover,
+Widget myHomePageState(List<String> imageList) {
+  return Container(
+    margin: EdgeInsets.all(12),
+    child: StaggeredGridView.countBuilder(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 12,
+        itemCount: imageList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(imageList[index]),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-        );
-      },
-      staggeredTileBuilder: (index) {
-        return StaggeredTile.count(1, index.isEven ? 1.2 : 1.8);
-      });
+          );
+        },
+        staggeredTileBuilder: (index) {
+          return StaggeredTile.count(1, index.isEven ? 1.2 : 1.8);
+        }),
+  );
 }
