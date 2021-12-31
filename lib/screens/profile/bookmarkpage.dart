268b79066cae56a9148f42491df191e31ea4541c
potato0520 <potato20040520@gmail.com>
2021-12-31 23:08:19 +0900

    [수정] Bookmarkpage 프로필 페이지에 추가

---
+++ lib/screens/profile/bookmarkpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookMarkPage extends StatefulWidget {
+  const BookMarkPage({Key? key}) : super(key: key);
+
+  @override
+  _BookMarkPageState createState() => _BookMarkPageState();
+}
+
+class _BookMarkPageState extends State<BookMarkPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
