bb61929add99fb6e53a5c21ef880124d7c3f24f5
potato0520 <potato20040520@gmail.com>
2022-01-11 23:07:16 +0900

    🖋️ :: change new appBar

--- lib/screens/recipe_share/detail_recipe1.dart
+++ lib/screens/recipe_share/detail_recipe1.dart
@@ -10,15 +10,19 @@ class DetailRecipe1 extends StatefulWidget {
 
 class _DetailRecipe1State extends State<DetailRecipe1> {
   @override
+  var pageName = 'Recipe';
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
         children: [
-          appBar(
+          styleAppBar(
             context,
-            text: '',
-            icon: Icons.bookmark_border,
+            styleAppBarIcon2: Icons.more_horiz,
+            styleAppBarIconText2: '',
+            styleAppBarIcon: Icons.bookmark_border_outlined,
+            styleAppBarIconText: '',
+            styleAppBarTitle: pageName,
           )
         ],
       ),
