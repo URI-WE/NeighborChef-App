617acbd3ceaa03eeee6ec500f36e905b583079a8
potato0520 <potato20040520@gmail.com>
2022-01-12 22:49:17 +0900

    💡 :: publising complete

--- lib/screens/recipe_share/new_post_share_recipe.dart
+++ lib/screens/recipe_share/new_post_share_recipe.dart
@@ -34,7 +34,8 @@ class _NewPostShareRecipeState extends State<NewPostShareRecipe> {
               styleAppBarIconText: 'next',
               styleAppBarIconTextColor: Colors.red,
               styleAppBarTitle: pageName,
-              styleAppBarNavigate: null),
+              styleAppBarNavigate: null,
+              styleAppBarIcon2: null),
           Padding(
             padding: EdgeInsets.all(5),
             child: Row(
@@ -70,93 +71,185 @@ class _NewPostShareRecipeState extends State<NewPostShareRecipe> {
           ),
           Padding(
             padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                fillColor: Color(0xffF6F6F6),
-                filled: true,
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 4),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    )),
-                labelText: 'Recipe Name',
+            child: Center(
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Color(0xffF6F6F6),
+                  filled: true,
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xffF6F6F6), width: 4.0),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFFF6F6F6), width: 4.0),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  hintText: 'Recipe Name',
+                ),
               ),
             ),
           ),
           Padding(
             padding: EdgeInsets.all(15),
             child: Container(
-              color: Color(0xffF6F6F6),
+              decoration: BoxDecoration(
+                  color: Color(0xffF6F6F6),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
               child: Column(
                 children: [
+                  Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          child: Container(
+                              width: 100,
+                              height: 40,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.update,
+                                    size: 40,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text('total time'),
+                                      Text('Pick'),
+                                    ],
+                                  )
+                                ],
+                              )),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          child: Container(
+                              width: 100,
+                              height: 40,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.directions_run,
+                                    size: 40,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text('difficulty'),
+                                      Text('Pick'),
+                                    ],
+                                  )
+                                ],
+                              )),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 0.5,
+                    width: MediaQuery.of(context).size.width * 4 / 5,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 80,
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            fillColor: Color(0xffE2E2E2),
+                            filled: true,
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xffE2E2E2), width: 4.0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xFFE2E2E2), width: 4.0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                            hintText: 'Category',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 1,
+                      height: 100,
+                      color: Color(0xffE2E2E2),
+                      child: Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            fillColor: Color(0xffE2E2E2),
+                            filled: true,
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xffE2E2E2), width: 4.0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xFFE2E2E2), width: 4.0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                            hintText: 'Category',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Container(
-                            width: 100,
-                            height: 40,
-                            child: Row(
-                              children: [
-                                Icon(
-                                  Icons.update,
-                                  size: 40,
-                                ),
-                                Column(
-                                  children: [
-                                    Text('total time'),
-                                    Text('Pick'),
-                                  ],
-                                )
-                              ],
-                            )),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Container(
-                            width: 100,
-                            height: 40,
-                            child: Row(
-                              children: [
-                                Icon(
-                                  Icons.directions_run,
-                                  size: 40,
-                                ),
-                                Column(
-                                  children: [
-                                    Text('difficulty'),
-                                    Text('Pick'),
-                                  ],
-                                )
-                              ],
-                            )),
+                        padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+                        child: Text(
+                          'Step',
+                          style: TextStyle(
+                              fontSize: 24, color: const Color(0xffFF7E55)),
+                        ),
                       )
                     ],
                   ),
                   Padding(
                     padding: EdgeInsets.all(20),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        fillColor: Color(0xffE2E2E2),
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.red, width: 4),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(15),
-                            )),
-                        labelText: 'Category',
+                    child: Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: Color(0xffE2E2E2),
+                          filled: true,
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(0xffE2E2E2), width: 4.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5))),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(0xFFE2E2E2), width: 4.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5))),
+                          hintText: 'Descript',
+                        ),
                       ),
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Container(
-                      width: 200,
-                      height: 100,
-                      color: Color(0xffE2E2E2),
+                    padding: EdgeInsets.all(5),
+                    child: styleButton(
+                      context,
+                      styleButtonIcon: Icons.image_search_outlined,
+                      styleButtonWidth: 78,
+                      styleButtonHeight: 50,
+                      styleButtonColor: Color(0xFFFFFFFF),
+                      styleButtonIconColor: Colors.amber,
+                      styleButtonTextColor: Color(0xFFFFFFFF),
+                      styleButtonBorderColor: Color(0xFFFFFFFF),
+                      styleButtonIconSize: 48,
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
