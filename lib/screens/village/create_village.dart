2edadd503c66bd61c3e79efcced632596f18ebbb
potato0520 <potato20040520@gmail.com>
2022-01-13 22:30:11 +0900

    ⚙️ :: make new page create_village

---
+++ lib/screens/village/create_village.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateVillage extends StatefulWidget {
+  const CreateVillage({Key? key}) : super(key: key);
+
+  @override
+  _CreateVillageState createState() => _CreateVillageState();
+}
+
+class _CreateVillageState extends State<CreateVillage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
