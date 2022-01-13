da10c5201de15925d961761f841dbaddeaf33fff
potato0520 <potato20040520@gmail.com>
2022-01-13 22:30:41 +0900

    ⚙️ make new page village_profile

---
+++ lib/screens/village/village_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VillageProfile extends StatefulWidget {
+  const VillageProfile({Key? key}) : super(key: key);
+
+  @override
+  _VillageProfileState createState() => _VillageProfileState();
+}
+
+class _VillageProfileState extends State<VillageProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
