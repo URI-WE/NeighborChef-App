dd8cb7be1daf824dc264f422576b201688354613
Matthew <openwind125@gmail.com>
2022-01-27 21:36:00 +0900

    Add bookmark.dart

---
+++ lib/screens/profile/bookmark.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:neighborchefapp/widgets/etc/piclist.dart';
+import 'package:http/http.dart' as http;
+
+class Bookmark extends StatefulWidget {
+  @override
+  _BookmarkState createState() => _BookmarkState();
+}
+
+class _BookmarkState extends State<Bookmark> {
+  var _text = "Http Example";
+  List<Data> _datas = []; // get image url from server
+
+  Future _fetchPosts() async {
+    final response = await http.get(Uri.parse("URL"));
+    _text = utf8.decode(response.bodyBytes);
+    var dataObjsJson = jsonDecode(_text)['post'] as List;
+    final List<Data> parsedResponse =
+        dataObjsJson.map((dataJson) => Data.fromJson(dataJson)).toList();
+    setState(() {
+      _datas.clear();
+      _datas.addAll(parsedResponse);
+    });
+    print(parsedResponse);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: <Widget>[
+            appBar(context, pagename: 'My Bookmark'),
+            myHomePageState(_datas), // it works even if you put a list of image urls
+          ],
+        ),
+      ),
+    );
+  }
+}
